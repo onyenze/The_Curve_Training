1001238fb2492e8c82035f6572680e0b2e9464ae
onyenze <chibuezeonyeze123@gmail.com>
2023-05-31 10:45:54 +0100

    working on assignmnets

--- API with Nested Object/Election_API/server.js
+++ API with Nested Object/Election_API/server.js
@@ -20,17 +20,176 @@ app.get("/",(req,res)=>{
     res.send("Welcome to my Election API")
 })
 
+
+// create an entry
 app.post("/create",async (req,res)=>{try {
     const newEntry = await electionModel.create(req.body)
     res.status(200).json({data:newEntry})
-
 } catch (error) {
     console.log(error.message)
 }
-    
 })
 
+// get all the entries
+app.get("/election", async (req, res) => {
+    try {
+      const entry = await electionModel.find();
+      if (!entry) {
+        res.status(404).json({
+          message: "database not found",
+        });
+      } else if (entry.length == 0) {
+        res.status(200).json({
+          message: "no entry on the database",
+        });
+      } else {
+        res.status(200).json({
+          message: "successful",
+          data: entry,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
+    }
+  });
 
+//getting entry based on ID
+app.get("/election/:id", async (req, res) => {
+    try {
+      const entryId = req.params.id;
+      const entry = await electionModel.findById(entryId);
+      if (!entry) {
+        res.status(404).json({
+          message: "entry not found",
+        });
+      } else {
+        res.status(200).json({
+          message: "successful",
+          data: entry,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
+    }
+  });
+
+
+//updating entry based on ID
+app.put("/election/:id", async (req, res) => {
+    try {
+      const entryId = req.params.id;
+      const updatedEntry = await electionModel.findByIdAndUpdate(entryId, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      if (!updatedEntry) {
+        res.status(404).json({
+          message: "entry not found",
+        });
+      } else {
+        res.status(200).json({
+          message: "entry updated",
+          data: updatedEntry,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
+    }
+  });
+
+
+//deleting entry
+app.delete("/election/:id", async (req, res) => {
+    try {
+      const entryId = req.params.id;
+      const deletedEntry = await electionModel.findByIdAndDelete(entryId);
+      if (!deletedEntry) {
+        res.status(404).json({
+          message: "entry not found",
+        });
+      } else {
+        res.status(200).json({
+          message: "entry deleted",
+          data: deletedEntry,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
+    }
+  });
+
+// Getting where the election is rigged
+app.get("/electionRigged", async (req, res) => {
+    try {
+      const riggedEntry = await electionModel.find({isRigged:true});
+      if (!riggedEntry) {
+        res.status(404).json({
+          message: "database not found",
+        });
+      } else if (riggedEntry.length == 0) {
+        res.status(200).json({
+          message: "no entry on the database",
+        });
+      } else {
+        res.status(200).json({
+          message: "successful",
+          data: riggedEntry,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
+    }
+  });
+
+
+// find where we have double collation officer
+app.get("/collation/:collation", async (req, res) => {
+    try {
+      const entry = await electionModel.find({colationOfficer :req.params.collation,});
+      if (entry.length<1) {
+        res.status(404).json({
+          message: "not an officer",
+        });
+      } else if (entry.length>1) {
+        res.status(200).json({
+          message: "Worked on two states",
+          data: entry,
+        });
+      }
+      else {res.status(200).json({
+        message: "Worked on one state",
+        data: entry
+      })}
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
+    }
+  });
+// to delete the rigged elections
+app.delete("/riggedElection", async (req,res)=>{
+    try {
+        const riggedElection = await electionModel.find({isRigged:true})
+        const deletedEntry = await electionModel.deleteMany({isRigged:true})
+        res.status(200).json({
+            message:"These are the rigged Elections",
+            data:riggedElection,
+            status:deletedEntry
+        })
+    } catch (error) {
+        console.log(error.message)
+    }
+})
 
 
 
