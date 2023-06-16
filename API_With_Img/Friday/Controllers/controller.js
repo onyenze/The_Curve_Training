42adb8960e3558f17cd343e8525d9cdf9da3edfa
onyenze <chibuezeonyeze123@gmail.com>
2023-06-16 20:53:06 +0100

    redo

--- API_With_Img/Friday/Controllers/controller.js
+++ API_With_Img/Friday/Controllers/controller.js
@@ -32,31 +32,16 @@ const myCloudinary = require("../Config/cloudinary")
 
 
 const newContact = async (req,res) => {
+    const picture = req.files
+    try {
+       
+    await myCloudinary.uploader.upload("req.files.ProfilePicture.tempFilePath",(err,ProfilePicture)=>{
+        try {
+            return ProfilePicture
+        } catch (error) {
+            res.status(500).json({message:error.message})
+        }})
 
-    try{
-      
-
-            // Use the uploaded file's name as the asset's public ID and 
-            // allow overwriting the asset with new versions
-            const options = {
-              use_filename: true,
-              unique_filename: false,
-              overwrite: true,
-            }
-
-
-         const result = await myCloudinary.uploader.upload(req.imageUrl, (error, resi) => {
-            if (error) {
-              console.log('Error:', error);
-            } else {
-              // Retrieve the secure URL of the uploaded file
-              const secureUrl = resi.secure_url;
-              console.log('Uploaded file URL:',result);
-        
-              // Perform further processing with the file URL if needed
-              // ...
-            }
-          });
         
     } catch (error) {
         res.status(200).json({message:error.message})
