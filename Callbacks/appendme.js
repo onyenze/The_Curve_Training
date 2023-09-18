d17a5301de4d01e6da453ffc19bba663e37271a2
onyenze <chibuezeonyenze123@gmail.com>
2023-09-18 23:25:15 +0100

    callbacks

--- Callbacks/appendme.js
+++ Callbacks/appendme.js
@@ -3,4 +3,7 @@ fs.appendFile("hardWork.html"," still grinding",(error)=>{
     if(error){console.log(error)}
     else{console.log(" stuffs have been added")}
 
+
+    // I am trying to keep up with myself
+    // It has not being easy
 })
